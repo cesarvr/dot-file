adb32ef5e9536ef7e8fd8e8bdcaa8ffa1428aa7c
cesar <cesarv01@yahoo.com>
2017-03-04 17:13:44 +0000

    adding tmp folder creation

--- vim_install.sh
+++ vim_install.sh
@@ -4,7 +4,11 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vi
 #install vundle-vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+#creating temporary directory
+mkdir ~/.tmp
+
 #install dot-vim 
 cp .vimrc ~/.vimrc 
 vim -c ":PluginInstall"
+
 

b4ca6c7a6da9a57972149453da1860fb6e6d8d12
Cesar <cesarv01@yahoo.com>
2017-04-29 16:16:09 +0100

    installing YouCompleteMe

--- vim_install.sh
+++ vim_install.sh
@@ -9,10 +9,23 @@ mkdir ~/.tmp
 
 #YouCompleteMe vim plugin 
 git clone https://github.com/Valloric/YouCompleteMe ~/.vim/bundle/YouCompleteMe
+
+
+#Getting YouCompleteMe Deps 
+SAVE_FOLDER=${PWD}
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+
+#returning to install folder
+cd $SAVE_FOLDER
+
+#YouCompleteMe installer
 python ~/.vim/bundle/YouCompleteMe/install.py --clang-completer --system-libclang  
+
 
 #install dot-vim 
 cp .vimrc ~/.vimrc 
 vim -c ":PluginInstall"
+
 
 

03d156be99c7b6e94da89d5962b679178bdebd1e
cvaldezr <cvaldezr@redhat.com>
2017-09-10 12:17:54 +0100

    adding vulkan api to installer

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sh install-repos.sh
 
 
 #initial packages
-sudo dnf install -y util-linux-user zsh sqlite gconf-editor.x86_64 steam bumblebee-nvidia bbswitch-dkms VirtualGL.x86_64 VirtualGL.i686 primus.x86_64 primus.i686 kernel-devel clang cmake playonlinux gnome-tweak-tool playonlinux.x86_64 google-droid-kufi-fonts.noarch google-droid-sans-fonts.noarch google-droid-serif-fonts.noarch google-droid-sans-mono-fonts.noarch python2-pygpgme.x86_64 dropbox transmission-gtk.x86_64 numix-icon-theme-circle-0.1.0-12.gitc3aefdb.fc26.noarch arc-theme-20170302-4.fc26.noarch python2-devel-2.7.13-10.fc26.i686
+sudo dnf install -y util-linux-user zsh sqlite gconf-editor.x86_64 steam bumblebee-nvidia bbswitch-dkms VirtualGL.x86_64 VirtualGL.i686 primus.x86_64 primus.i686 kernel-devel clang cmake playonlinux gnome-tweak-tool playonlinux.x86_64 google-droid-kufi-fonts.noarch google-droid-sans-fonts.noarch google-droid-serif-fonts.noarch google-droid-sans-mono-fonts.noarch python2-pygpgme.x86_64 dropbox transmission-gtk.x86_64 numix-icon-theme-circle-0.1.0-12.gitc3aefdb.fc26.noarch arc-theme-20170302-4.fc26.noarch python2-devel-2.7.13-10.fc26.i686 mesa-vulkan-drivers mesa-vulkan-devel
 
 
 #oh-my-zsh
